cf8923b114ca0c6b0fdc70ff0c6f905c7cbb7ae2
sellaziz <59337531+sellaziz@users.noreply.github.com>
2023-10-14 22:40:56 +0200

    src: add search features
    User can now search for a word in the text through typing '/' followed
    by the word that is searched. The word is highlighted and the user can
    loop through them using 'n' or 'N'.

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,14 @@ use std::{fs, io};
 #[derive(Debug)]
 pub enum AppStates {
     Running,
+    FindStr,
     TypeCmd,
+}
+
+pub struct Finder {
+    pub found_items: Vec<(usize, usize)>,
+    pub idx: Option<usize>,
+    pub word: Option<String>,
 }
 
 pub struct App<'a> {
@@ -16,6 +23,34 @@ pub struct App<'a> {
     pub line_count: usize,
     pub state: AppStates,
     pub cmd_str: String,
+    pub finder: Finder,
+}
+
+impl Finder {
+    pub fn new() -> Result<Finder, io::Error> {
+        Ok(Finder{
+            found_items: Vec::new(),
+            idx: None,
+            word: None,
+        })
+    }
+
+    pub fn search_case_insensitive_all(&mut self, contents: & Vec<String>) -> Option<bool>{
+        let mut found: bool = false;
+        let mut idx = 0;
+
+        let query = self.word.clone().unwrap().to_lowercase();
+
+        for line in contents {
+            if let Some(index) = line.to_lowercase().find(&query) {
+                found = true;
+                self.found_items.push((idx, index));
+            }
+            idx+=1;
+        }
+
+        Some(found)
+    }
 }
 
 impl<'a> App<'a> {
@@ -35,6 +70,7 @@ impl<'a> App<'a> {
             line_count: line_count,
             state: AppStates::Running,
             cmd_str: "".to_string(),
+            finder: Finder::new().unwrap(),
         })
     }
 
@@ -63,13 +99,41 @@ impl<'a> App<'a> {
                     'k' => {
                         self.on_up();
                     }
+                    'n' => {
+                        if let Some(mut cur_id) = self.finder.idx {
+                            if cur_id+1 < self.finder.found_items.len() {
+                                cur_id+=1;
+                                self.finder.idx = Some(cur_id);
+                            }
+                            self.current_id = self.finder.found_items[cur_id].0;
+                        } else {
+                            self.finder.idx = Some(0);
+                            self.current_id = self.finder.found_items[0].0;
+                        }
+                    }
+                    'N' => {
+                        if let Some(mut cur_id) = self.finder.idx {
+                            if cur_id-1 > 0 {
+                                cur_id-=1;
+                                self.finder.idx = Some(cur_id);
+                            }
+                            self.current_id = self.finder.found_items[cur_id].0;
+                        } else {
+                            self.finder.idx = Some(0);
+                            self.current_id = self.finder.found_items[0].0;
+                        }
+                    }
                     '/' => {
+                        self.cmd_str = "".to_string();
+                        self.state = AppStates::FindStr;
+                    }
+                    ':' => {
                         self.state = AppStates::TypeCmd;
                     }
                     _ => {}
                 }
             }
-            AppStates::TypeCmd => {
+            AppStates::TypeCmd | AppStates::FindStr => {
                 self.cmd_str.push(c);
             }
         }
@@ -78,10 +142,16 @@ impl<'a> App<'a> {
     pub fn on_enter(&mut self) {
         match self.state {
             AppStates::Running => {}
+            AppStates::FindStr => {
+                self.finder.word = Some(self.cmd_str.clone());
+                self.finder.search_case_insensitive_all(&self.adjusted_file_contents);
+                self.state = AppStates::Running;
+            }
             AppStates::TypeCmd => {
                 self.state = AppStates::Running;
             }
         }
     }
+
     pub fn on_tick(&mut self) {}
 }
--- src/ui.rs
+++ src/ui.rs
@@ -31,13 +31,27 @@ pub fn render_cmdline<W>(w: &mut W, app: &mut App) -> io::Result<()>
 where
     W: io::Write,
 {
-    let (_column_size, line_size) = terminal::size().unwrap();
+    let (column_size, line_size) = terminal::size().unwrap();
     let cmd_str = app.cmd_str.clone();
+    let status:String;
+    if let Some(idx) = app.finder.idx {
+        status = format!("search:({}/{}),({},{}):{:3}%",
+            idx, app.finder.found_items.len(),
+            app.current_id, app.line_count, ((app.current_id as f32/app.line_count as f32)*100.0) as u16);
+
+    } else {
+        status = format!("({},{}):{:3}%",app.current_id, app.line_count, ((app.current_id as f32/app.line_count as f32)*100.0) as u16);
+    }
 
     execute!(
         w,
+        style::SetForegroundColor(Color::Black),
+        style::SetBackgroundColor(Color::White),
         cursor::MoveTo(0, line_size),
         style::Print(cmd_str),
+        cursor::MoveTo(column_size - status.len() as u16, line_size),
+        style::Print(status),
+        style::ResetColor,
     )?;
 
     Ok(())
@@ -57,22 +71,40 @@ where
 
     w.flush()?;
     let mut current_line = 0;
-    for line in limit_line_length(app.file_contents.lines(), (column_size - 1) as usize) {
+    for line in &app.adjusted_file_contents {
         current_line += 1;
         if current_line < app.current_id {
             continue;
         }
         if cursor::position()?.1 < line_size - 1 {
-            write!(w, "{}", line)?;
+            execute!(w, style::ResetColor,)?;
+            if let Some(cur_word) = app.finder.word.clone() {
+                let word = cur_word.clone();
+                if let Some(index) = line.to_lowercase().find(&word[..]) {
+                    for (i, c) in line.char_indices() {
+                        if i >= index {
+                            if i >= index + word.len() {
+                                execute!(w, style::ResetColor,)?;
+                            } else {
+                                execute!(
+                                    w,
+                                    style::SetForegroundColor(Color::Black),
+                                    style::SetBackgroundColor(Color::White),
+                                )?;
+                            }
+                        }
+                        write!(w, "{}", c)?;
+                    }
+                } else {
+                    write!(w, "{}", line)?;
+                }
+            } else {
+                write!(w, "{}", line)?;
+            }
             execute!(w, cursor::MoveToNextLine(1),).unwrap();
         }
     }
-    execute!(
-        w,
-        style::SetForegroundColor(Color::Black),
-        style::SetBackgroundColor(Color::White),
-    )?;
-    render_cmdline(w, app);
+    let _ = render_cmdline(w, app);
     Ok(())
 }
 
